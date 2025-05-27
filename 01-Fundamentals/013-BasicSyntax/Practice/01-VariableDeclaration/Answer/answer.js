052b904f5e9f4cbb06c51893817b7dd0ab181258
rehanzhaky <hannn9491@gmail.com>
2025-05-27 15:40:43 +0700

    feat: add correct  answer about variable declaration

---
+++ 01-Fundamentals/013-BasicSyntax/Practice/01-VariableDeclaration/Answer/answer.js
@@ -0,0 +1,11 @@
+// Declare a `const` variable named `favoriteColor` and assign it your favorite color as a string.
+const favoriteColor = "Red";
+// Declare a `let` variable named `age` and assign it your age as a number.
+let age = 23;
+// Declare a `let` variable named `isStudent` and assign it a boolean value based on whether you are currently a student.
+let isStudent = true;
+
+// Print the values of all three variables to the console.
+console.log(favoriteColor);
+console.log(age);
+console.log(isStudent);
