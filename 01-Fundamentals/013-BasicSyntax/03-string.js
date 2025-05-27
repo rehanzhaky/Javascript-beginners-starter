fd4405d34a99b618e1908b06ed3abf7bb50453c1
rehanzhaky <hannn9491@gmail.com>
2025-05-27 15:01:02 +0700

    feat: add basic syntax string

---
+++ 01-Fundamentals/013-BasicSyntax/03-string.js
@@ -0,0 +1,32 @@
+// Assign the string values using double quotes
+let address = "Indonesia street, number 04";
+// Assign the string values using single quotes
+let description = "This is my parents home address";
+
+// Output: Indonesia street, number 04
+console.log(address);
+// Output: This is my parents home address
+console.log(description);
+
+// Practice using template literals
+// Assign the value of variable
+let firstName = "Ree";
+// Assign the string values using backticks, template literals
+// Allow u to embed variables directly into the string using ${variableName}
+let opening = `Hello ${firstName}, welcome to the party 🎂`;
+// prints the value of opening
+console.log(opening);
+
+// String operations
+// Concatenation
+let greeting = "Hello";
+let guessName = "Jack";
+// Concatenate the string (+)
+let intro = greeting + ", " + guessName + "!";
+console.log(intro);
+
+
+// String Length
+let fullName = "Raihan Zhaki";
+// using the .length property
+console.log(fullName.length);
