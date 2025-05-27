d56272a2d91326e65a2868277b05adee5cffcced
rehanzhaky <hannn9491@gmail.com>
2025-05-27 14:33:22 +0700

    feat: add basic syntax variable let

--- 01-Fundamentals/012-CodeEditor&Browser/script.js
+++ 01-Fundamentals/012-CodeEditor&Browser/script.js
@@ -1,1 +1,2 @@
 console.log("Hello World, this is me 👋");
+console.log("This is me again ✌️")
---
+++ 01-Fundamentals/013-BasicSyntax/let.js
@@ -0,0 +1,13 @@
+// Use let when u know the value of the variable might change during the execution of ur program.
+// Declares a variable name "age"
+let age; 
+// Assigns a value to the variable
+age = 23;
+// Prints the value of the variable
+console.log(age);
+
+// Year Changes
+// Reassigns a value to the variable
+age = 24;
+// Prints the value of the variable
+console.log(age);
