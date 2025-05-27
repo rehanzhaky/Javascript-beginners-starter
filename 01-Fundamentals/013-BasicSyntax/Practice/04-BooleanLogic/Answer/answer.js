7cc9c72fb49ead75618796467393f4ebff35ce6d
rehanzhaky <hannn9491@gmail.com>
2025-05-27 15:54:18 +0700

    feat: add correct  answer about boolean logic

---
+++ 01-Fundamentals/013-BasicSyntax/Practice/04-BooleanLogic/Answer/answer.js
@@ -0,0 +1,9 @@
+// Create two variables: `hasLicense` and `isInsured`, and assign them boolean values based on whether you have a driver's license and car insurance.
+let hasLicense = true;
+let isInsured = false;
+
+// Create a variable `canDrive` that is true only if both `hasLicense` and `isInsured` are true.
+let canDrive = hasLicense && isInsured;
+
+// Print the value of `canDrive` to the console.
+console.log(canDrive);
