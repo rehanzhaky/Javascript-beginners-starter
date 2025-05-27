2184cf521c26adf3cda34821702d19b28a53c508
rehanzhaky <hannn9491@gmail.com>
2025-05-27 15:08:48 +0700

    feat: add basic syntax number

---
+++ 01-Fundamentals/013-BasicSyntax/04-number.js
@@ -0,0 +1,36 @@
+// Numbers
+// Javascript has only one type of number. Numbers can be written with or without decimals.
+// Integer (without decimals)
+let a = 10;
+// Floating point number (with decimals)
+let b = 10.5;
+
+// Output: 10
+console.log(a);
+// Output: 10.5
+console.log(b);
+
+
+// Number Operations
+// Addition
+let c = 10 + 5;
+// Output: 15
+console.log(c);
+
+// Subtraction
+let d = 10 - 5;
+// Output: 5
+console.log(d);
+
+// Multiplication
+let e = 10 * 5;
+// Output: 50
+console.log(e);
+
+// Division
+let f = 10 / 5;
+// Output: 2
+
+// Modulus
+let g = 10 % 5;
+// Output: 0
