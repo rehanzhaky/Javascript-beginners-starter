6332d67c3068d45493fdf8e68caf0fe81f1cdc4a
rehanzhaky <hannn9491@gmail.com>
2025-05-27 15:51:19 +0700

    feat: add correct  answer about number operations

---
+++ 01-Fundamentals/013-BasicSyntax/Practice/03-NumberOperations/Answer/answer.js
@@ -0,0 +1,17 @@
+// Create two variables: `num1` and `num2`, and assign them any two numbers.
+let num1 = 11;
+let num2 = 10;
+
+// Calculate the sum, difference, product, and quotient of the two numbers.
+// Print the results of each operation to the console.
+let sum = num1 + num2;
+console.log("Sum: " + sum);
+
+let product = num1 * num2;
+console.log("Product: " + product);
+
+let difference = num1 - num2;
+console.log("Difference: " + difference);
+
+let quotient = num2 / num1;
+console.log("Quotient: " + quotient);
