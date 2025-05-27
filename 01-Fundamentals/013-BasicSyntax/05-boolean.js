b1be9fb1c20b48ffb12ed6796fc8707c6f678d4a
rehanzhaky <hannn9491@gmail.com>
2025-05-27 15:16:53 +0700

    feat: add basic syntax boolean

---
+++ 01-Fundamentals/013-BasicSyntax/05-boolean.js
@@ -0,0 +1,41 @@
+// Basic Booleans
+// The value of a boolean is either true or false.
+
+let isWomen = true;
+let isMen = false;
+
+// Output: true
+console.log(isWomen);
+// Output: false
+console.log(isMen);
+
+// Boolean Operations
+
+// Comparison Operators
+let x = 10;
+let y = 8;
+
+// Output: true
+console.log(x > y);
+// Output: false
+console.log(x < y);
+// Output: false
+console.log(x == y);
+// Output: true
+console.log(x != y);
+// Output: true
+console.log(x >= y);
+// Output: false
+console.log(x <= y);
+
+// Logical Operators
+let p = true;
+let q = false;
+
+// Output: true
+console.log(p && q);
+// Output: true
+console.log(p || q);
+// Output: false
+console.log(!p);
+
